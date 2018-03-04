42addb61c0a5749d0ecb34e3d6f61e9f0c00be5c
Jonathan Shobrook <shobrookj@gmail.com>
2018-03-04 03:56:33 -0600

    Added example usage for new util functions

--- rebound.py
+++ rebound.py
@@ -1,14 +1,26 @@
+# Globals #
+
+
 import click
 import utilities as util
+
+
+# Main #
+
 
 @click.command()
 @click.argument("command")
 def rebound(command):
     output, error = util.execute(command) # Excutes the command and pipes output
-    error = util.isolate_error(error) # Isolates the error message
+    language = util.get_language(command) # Gets the language name
+    #error_msg = util.get_error_message(error) # Prepares error message for search
+    search_results, last_page = util.search_stackoverflow("TypeError: integer", 1)
+    print(search_results)
 
     if error != None:
-        # TODO: Scrape Stack Overflow results for the error message
-        if click.confirm('Display Stack Overflow results?'):
+        #search_results, last_page = util.search_stackoverflow(language + " " + error_msg, 1)
+        if click.confirm("Display Stack Overflow results?"):
             click.echo("Hah!")
-            # TODO: Display results in a user-friendly manner
+            # TODO: Display search results in a user-friendly manner
+            #if last_page and click.confirm("Display more results?"):
+                #click.echo(":^)")
