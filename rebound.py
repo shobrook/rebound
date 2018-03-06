4b23fd383e8d9907a221b6eb7339244e9c92ca8e
Jonathan Shobrook <shobrookj@gmail.com>
2018-03-05 20:50:35 -0600

    Added a control flow for displaying search results

--- rebound.py
+++ rebound.py
@@ -1,8 +1,22 @@
 # Globals #
 
 
+import os
 import click
 import utilities as util
+
+
+# Helpers #
+
+
+def display_results(search_results):
+    for result in search_results:
+        color = "green" if result["Answers"] != 0 else "white"
+
+        click.echo(click.style("(" + str(result["Votes"]) + " Votes | " + str(result["Answers"]) + " Answers) ", fg=color) + click.style(result["Title"], bold=True))
+        click.echo(result["Body"])
+        click.echo(click.style("\n" + result["URL"], fg="blue"))
+        click.echo(os.get_terminal_size().columns * "–") # Display divider
 
 
 # Main #
@@ -14,13 +28,25 @@ def rebound(command):
     output, error = util.execute(command) # Excutes the command and pipes output
     language = util.get_language(command) # Gets the language name
     #error_msg = util.get_error_message(error) # Prepares error message for search
-    search_results, last_page = util.search_stackoverflow("TypeError: integer", 1)
-    print(search_results)
 
-    if error != None:
-        #search_results, last_page = util.search_stackoverflow(language + " " + error_msg, 1)
-        if click.confirm("Display Stack Overflow results?"):
-            click.echo("Hah!")
-            # TODO: Display search results in a user-friendly manner
-            #if last_page and click.confirm("Display more results?"):
-                #click.echo(":^)")
+    if error != None: # error_msg
+        click.echo(os.get_terminal_size().columns * "–") # Display divider
+
+        search_results, last_page, captcha = util.search_stackoverflow("Example error message", 1) # language + " " + error_msg
+        if search_results != []:
+            if click.confirm("\nDisplay Stack Overflow results?"):
+                if captcha:
+                    click.echo(click.style("\nSorry, Stack Overflow blocked our request. Try again in a minute.", fg="red"))
+                else:
+                    click.echo(click.style("\nResults for: " + "Example error message" + "\n", bold=True)) # (language + " " + error_msg)
+                    display_results(search_results)
+
+                    count = 1
+                    while click.confirm("Display more results?") and not last_page:
+                        search_results, last_page, captcha = util.search_stackoverflow("Example error message", count + 1) # language + " " + error_msg
+
+                        if captcha:
+                            click.echo(click.style("\nSorry, Stack Overflow blocked our request. Try again in a minute.", fg="red"))
+                            break
+                        else:
+                            display_results(search_results)
