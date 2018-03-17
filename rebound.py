f050ba4a2f04674230b5f5a19c78aae50935228a
Jonathan Shobrook <shobrookj@gmail.com>
2018-03-16 23:27:55 -0500

    Moved from Click to urwid

--- rebound.py
+++ rebound.py
@@ -1,56 +1,27 @@
-# Globals #
-
-
+import sys
 import os
-import click
+#import click
 import utilities as util
 
 
-# Helpers #
-
-
-def display_results(search_results):
-    for result in search_results:
-        color = "green" if result["Answers"] != 0 else "white"
-
-        click.echo(click.style("(" + str(result["Votes"]) + " Votes | " + str(result["Answers"]) + " Answers) ", fg=color) + click.style(result["Title"], bold=True))
-        click.echo(result["Body"])
-        click.echo(click.style("\n" + result["URL"], fg="blue"))
-        click.echo(os.get_terminal_size().columns * "–") # Display divider
-
-    # TODO: Reformat the answer thumbnails
-
-
-# Main #
-
-
-@click.command()
-@click.argument("command")
-def rebound(command):
-    output, error = util.execute(command) # Excutes the command and pipes output
-    language = util.get_language(command) # Gets the language name
+def rebound(file_path):
+    language = util.get_language(file_path) # Gets the language name
+    output, error = util.execute("%s %s" % (language, file_path)) # Executes the command and pipes stdout
     error_msg = util.get_error_message(error, language) # Prepares error message for search
 
     if error_msg != None:
-        click.echo("")
-        click.echo(os.get_terminal_size().columns * "–") # Display divider
+        sys.stdout.write("\n" + os.get_terminal_size().columns * "-") # Display divider
 
-        query = language + " " + error_msg
-        search_results, last_page, captcha = util.search_stackoverflow(query, 1) # language + " " + error_msg
+        query = "%s %s" % (language, error_msg)
+        search_results, last_page, captcha = util.search_stackoverflow("How to use raw input?", 1)
+
         if search_results != []:
-            if click.confirm("\nDisplay Stack Overflow results?"):
+            if util.query_display_results("Display Stack Overflow results?"):
                 if captcha:
-                    click.echo(click.style("\nSorry, Stack Overflow blocked our request. Try again in a minute.", fg="red"))
+                    sys.stdout.write("\nSorry, Stack Overflow blocked our request. Try again in a minute.") # TODO: Make this red
                 else:
-                    click.echo(click.style("\nResults for: " + query + "\n", bold=True)) # (language + " " + error_msg)
-                    display_results(search_results)
+                    util.display_results(search_results, query, last_page)
+        else:
+            sys.stdout.write("\nNo Stack Overflow results found.")
 
-                    count = 1
-                    while click.confirm("Display more results?") and not last_page:
-                        search_results, last_page, captcha = util.search_stackoverflow(query, count + 1) # language + " " + error_msg
-
-                        if captcha:
-                            click.echo(click.style("\nSorry, Stack Overflow blocked our request. Try again in a minute.", fg="red"))
-                            break
-                        else:
-                            display_results(search_results)
+rebound(sys.argv[1]) # TODO: Handle multiple arguments
