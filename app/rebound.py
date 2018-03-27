855c3edd2de703951899de597c756e5137afd513
Jonathan Shobrook <shobrookj@gmail.com>
2018-03-27 17:03:23 -0500

    Added time delays to prevent captchas

--- app/rebound.py
+++ app/rebound.py
@@ -2,6 +2,7 @@ import sys
 import os
 import utilities as util
 import webbrowser
+import time
 
 
 def rebound(command):
@@ -11,20 +12,30 @@ def rebound(command):
 
     if error_msg != None:
         query = "%s %s" % (language, error_msg)
-        search_results, last_page, captcha = util.search_stackoverflow(query, 1)
+        first_page_results, last_page, captcha = util.search_stackoverflow(query, 1)
 
-        if search_results != []:
+        if first_page_results != []:
             if captcha:
                 sys.stdout.write("\n" + util.RED + "Sorry, Stack Overflow blocked our request. Try again in a minute." + util.ENDC)
             elif util.confirm("\nDisplay Stack Overflow results?"):
                 sys.stdout.write("\n" + util.GRAY + os.get_terminal_size().columns * "-" + util.ENDC + "\n") # TODO: Make this responsive
 
-                util.display_first_result(search_results[0], query)
+                time.sleep(1)
+
+                util.display_first_result(first_page_results[0], query)
+
+                time.sleep(.5)
+
+                if not last_page:
+                    second_page_results, last_page, captcha = util.search_stackoverflow(query, 2)
 
                 sys.stdout.write("\n" + util.BOLD + util.BLUE + "Press ENTER for more choices, B to open in your browser, or any other key to quit: " + util.ENDC)
                 key = input().lower()
                 if key == "":
-                    util.display_all_results(search_results, query, 1, last_page)
+                    if captcha:
+                        sys.stdout.write("\n" + util.RED + "Sorry, Stack Overflow blocked our request. Try again in a minute." + util.ENDC)
+                        return
+                    util.display_all_results(first_page_results + second_page_results, query)
                 elif key == "b":
                     webbrowser.open(search_results[0]["URL"])
         else:
