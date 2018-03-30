e1d4719be02e223be833112f5586c587ad775dc1
Jonathan Shobrook <shobrookj@gmail.com>
2018-03-30 03:07:34 -0500

    Added check for unknown languages; removed divider

--- app/rebound.py
+++ app/rebound.py
@@ -12,6 +12,10 @@ import utilities as util
 
 def rebound(command):
     language = util.get_language(command[0].lower()) # Gets the language name
+    if language == "": # Unknown language
+        sys.stdout.write("\n" + util.RED + "Sorry, Rebound doesn't support this file type." + util.END)
+        return
+
     output, error = util.execute([language] + command[0:]) # Executes the command and pipes stdout
     error_msg = util.get_error_message(error, language) # Prepares error message for search
 
@@ -21,12 +25,11 @@ def rebound(command):
 
         if search_results != []:
             if captcha:
-                sys.stdout.write("\n" + util.RED + "Sorry, Stack Overflow blocked our request. Try again in a minute." + util.ENDC)
+                sys.stdout.write("\n" + util.RED + "Sorry, Stack Overflow blocked our request. Try again in a minute." + util.END)
+                return
             elif util.confirm("\nDisplay Stack Overflow results?"):
-                sys.stdout.write("\n" + util.GRAY + os.get_terminal_size().columns * "-" + util.ENDC + "\n") # TODO: Make this responsive
-
                 return util.display_all_results(search_results, query)
         else:
-            sys.stdout.write("\n" + util.RED + "No Stack Overflow results found." + util.ENDC)
+            sys.stdout.write("\n" + util.RED + "No Stack Overflow results found." + util.END)
 
 rebound(sys.argv[1:])
