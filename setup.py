cd67cbb4dc5d37e1bbca2dd825d766138dc09942
Jonathan Shobrook <shobrookj@gmail.com>
2018-05-17 02:28:43 -0400

    Updated for v1.1.9

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-try: from setuptools import setup
-except ImportError: from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 from codecs import open
 import sys
 
-if sys.version_info[:3] < (2, 0, 0):
-    sys.stdout.write("Python 1 is not supported.")
+if sys.version_info[:3] < (3, 0, 0):
+    sys.stdout.write("Requires Python 3 to run.")
     sys.exit(1)
 
 with open("README.md", encoding="utf-8") as file:
@@ -12,15 +14,14 @@ with open("README.md", encoding="utf-8") as file:
 
 setup(
     name="rebound-cli",
-    version="1.1.7a1",
-    description="Automatically displays Stack Overflow results when you get a compiler error",
-    long_description=readme,
-    long_description_content_type="text/markdown",
+    version="1.1.9a1",
+    description="Command-line tool that instantly fetches Stack Overflow results when you get a compiler error",
+    #long_description=readme,
+    #long_description_content_type="text/markdown",
     url="https://github.com/shobrook/rebound",
     author="shobrook",
     author_email="shobrookj@gmail.com",
     classifiers=[
-        "Development Status :: 3 - Alpha",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Topic :: Software Development",
@@ -32,10 +33,9 @@ setup(
     keywords="stackoverflow stack overflow debug debugging error-handling compile errors error message cli",
     include_package_data=True,
     packages=["rebound"],
-    #data_files=["demo.gif"],
     entry_points={"console_scripts": ["rebound = rebound.rebound:main"]},
     install_requires=["BeautifulSoup4", "requests", "urllib3", "urwid"],
     requires=["BeautifulSoup4", "requests", "urllib3", "urwid"],
-    python_requires=">=3", # NOTE: This will change
+    python_requires=">=3",
     license="MIT"
 )
