8a64a73e63dfe3d30b482c20860ca57d66881241
Daniel <ejedenials@gmail.com>
2021-12-01 02:10:47 +0100

    Update

--- rebound/test.py
+++ rebound/test.py
@@ -15,7 +15,6 @@ parser = argparse.ArgumentParser (prog='Rebound',description='Command-line tool 
 parser.add_argument('-v','--version', action='version', version='%(prog)s 2.0')
 parser.add_argument("-s","--script",help="Run Script from Terminal")
 parser.add_argument('-q','--query',help='Query stackoverflow with Error message ')
-#parser.add_argument('-e','--err',help='Specify path to error -> log.err file',action='store_true')
 subparser = parser.add_subparsers(dest='command')
 call = subparser.add_parser('call')
 call.add_argument("-id",'--pid',required=True)
@@ -23,15 +22,59 @@ call.add_argument('-e','--err',required=True)
 args = parser.parse_args()
 
 
-# if args.command=='call':
-#     if os.path.isfile(args.err):
-#         ProcessId= args.pid
-#         MonitorProcess()
+if args.command=='call':
+    if os.path.isfile(args.err):
+        ProcessId= args.pid
+        MonitorProcess()
+    else:
+        raise Exception("-e takes path to logfile Only")    
+elif args.query is not None:
+    search_results = search_google(args.query)
+    if search_results != []:
+        App(search_results) # Opens interface        
+    else:
+        print("\n%s%s%s" % (RED, "No Google results found.\n", END))
+
+elif args.script is not None:
+        language = get_language(args.script.lower()) # Gets the language name
+        if language == '': # Unknown language
+            print("\n%s%s%s" % (RED, "Sorry, Rebound doesn't support this file type.\n", END))
+            sys.exit(1)
+        file_path = args.script
+        if language == 'java':
+            file_path = [f.replace('.class', '') for f in file_path]
+        output, error = execute([language] + file_path) # Compiles the file and pipes stdout
+        if (output, error) == (None, None): # Invalid file
+            sys.exit(1)
+
+        error_msg = get_error_message(error, language) # Prepares error message for search
+        if error_msg != None:
+            language = 'java' if language == 'javac' else language # Fix language compiler command
+            site = 'site:stackoverflow.com'
+            query = "%s %s %s" % (language, error_msg,site)
+            #search_results, captcha = search_stackoverflow(query)
+            search_results = search_google(query)
+
+            if search_results != []:
+                # if captcha:
+                #     print("\n%s%s%s" % (RED, "Sorry, Stack Overflow blocked our request. Try again in a minute.\n", END))
+                #     return
+                #elif confirm("\nDisplay Stack Overflow results?"):
+                if confirm("\nDisplay Stack Overflow results?"):
+                    App(search_results) # Opens interface
+            else:
+                print("\n%s%s%s" % (RED, "No Google results found.\n", END))
+        else:
+            print("\n%s%s%s" % (CYAN, "No error detected :)\n", END))
+
+else: parser.print_help()
+
+     
+
 
 
 
     
-    #check if E is a file or a string path
 def execute_task(ProcessId):
   try:      
       while True:
@@ -72,31 +115,26 @@ def get_error_message(error, language):
             return None
         else:
             return error.split('\n')[-2].strip()
-def MonitorProcess():#ErrorLog,pid):
-  #ProcessState = execute_task(pid)
-  #clear terminal  
-  #with open(ErrorLog,'r') as log:
-  #  ErrorMessage = log.read()
-  ErrorMessage  = '\n \n Cant find module name scrappy \n '
-  ValidError= print(RED+BOLD+ErrorMessage,file=sys.stdout) if get_error_message(ErrorMessage,'python3') is None else get_error_message(ErrorMessage,'python3') 
-  print(ValidError)
-    #print to terminal and capture input while results are being fetched and cached
-    # ErrorMessage = ErrMessage.split('\n')
-  if ValidError is not None:
-        print(RED+BOLD+ErrorMessage,file=sys.stdout)
-        site = 'site:stackoverflow.com'
-        query = "%s %s %s" % ('python', ValidError,site)
-        search_results = search_google(query)
-        if search_results != []:
-            if confirm("\nDisplay Stack Overflow results?"):
-                #App(search_results) # Opens interface
-                #print([i['title'] for i in search_results])
-                print([result for result in search_results]) 
-        else:
-            print("\n%s%s%s" % (RED, "No Google results found.\n", END))
-  return 
-
-
-
-
-MonitorProcess()
+def MonitorProcess(ErrorLog,pid):
+    ProcessState = execute_task(pid)
+    #clear terminal  
+    with open(ErrorLog,'r') as log:
+        ErrorMessage = log.read()
+    #ErrorMessage  = '\n \n Cant find module name scrappy \n '
+    ValidError= print(RED+BOLD+ErrorMessage,file=sys.stdout) if get_error_message(ErrorMessage,'python3') is None else get_error_message(ErrorMessage,'python3') 
+    #print(ValidError)
+        #print to terminal and capture input while results are being fetched and cached
+        # ErrorMessage = ErrMessage.split('\n')
+    if ValidError is not None:
+            print(RED+BOLD+ErrorMessage,file=sys.stdout)
+            site = 'site:stackoverflow.com'
+            query = "%s %s %s" % ('python', ValidError,site)
+            search_results = search_google(query)
+            if search_results != []:
+                if confirm("\nDisplay Stack Overflow results?"):
+                    App(search_results) # Opens interface
+                    #print([i['title'] for i in search_results])
+                    print([result for result in search_results]) 
+            else:
+                print("\n%s%s%s" % (RED, "No Google results found.\n", END))
+    return 
